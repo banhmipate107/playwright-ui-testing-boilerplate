dc6c6ac6792d358b64245f1e739f8998101bbce5
banhmipate107 <ngkhavy172@gmail.com>
2025-04-24 09:18:51 +0700

    Fix test script and customized script

--- mock-data/user-data.ts
+++ mock-data/user-data.ts
@@ -1,18 +1,5 @@
 import { faker } from "@faker-js/faker";
 
-// const userData = {};
-// userData["name"] = faker.person.fullName();
-// userData["email"] = faker.internet.email();
-// userData["password"] = faker.internet.password();
-// userData["company"] = faker.company.name();
-// userData["website"] = faker.internet.url();
-// userData["country"] = faker.location.countryCode("alpha-2");
-// userData["city"] = faker.location.city();
-// userData["address1"] = faker.location.streetAddress();
-// userData["address2"] = faker.location.streetAddress();
-// userData["state"] = faker.location.state();
-// userData["zipcode"] = faker.location.zipCode();
-
 export const userData = {
   name: faker.person.fullName(),
   email: faker.internet.email(),
